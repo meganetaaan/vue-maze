546e1d974695d737af999fdd28a85de74e20a7a2
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2017-10-08 02:03:27 +0900

    #5 Get ready for npm publish

--- src/components/Maze/Maze.vue
+++ src/components/Maze/Maze.vue
@@ -11,76 +11,11 @@ import imagePath from './tori.png'
 import _ from 'lodash'
 import Maze from './getMaze'
 import Vue from 'vue'
-let seed = Date.now()
-
-class Renderer {
-  constructor(ctx, unitWidth, unitHeight, offset) {
-    this.ctx = ctx
-    this.unitWidth = unitWidth
-    this.unitHeight = unitHeight
-    this.offset = offset
-    this.wallWidth = 2
-  }
-
-  // TODO: 境界を見直す
-  clear(w, h) {
-    this.ctx.clearRect(0, 0, w, h)
-  }
-
-  setColor(fill, stroke) {
-    this.ctx.fillStyle = fill
-    this.ctx.strokeStyle = stroke
-  }
-
-  beginPath() {
-    this.ctx.beginPath()
-  }
-
-  stroke() {
-    this.ctx.stroke()
-  }
-
-  drawImage(x, y, image) {
-    const scaleX = this.unitWidth / image.width
-    const scaleY =  this.unitHeight / image.height
-    const cx = x * this.unitWidth / scaleX + this.offset
-    const cy = y * this.unitHeight / scaleY + this.offset
-    this.ctx.save()
-    this.ctx.scale(scaleX, scaleY)
-    this.ctx.drawImage(image, cx, cy)
-    this.ctx.restore()
-  }
-
-  drawCircle(x, y, r) {
-    this.ctx.beginPath()
-    const cx = x * this.unitWidth + this.unitWidth / 2 + this.offset
-    const cy = y * this.unitHeight + this.unitHeight / 2 + this.offset
-    r = r != null ? r : Math.min(this.unitWidth, this.unitHeight) / 2 - this.wallWidth
-    this.ctx.arc(cx, cy, r, 0, 2 * Math.PI)
-    this.ctx.fill()
-    this.ctx.stroke()
-  }
-
-  drawLine(x1, y1, x2, y2) {
-    const fromX = this.offset + x1 * this.unitWidth
-    const fromY = this.offset + y1 * this.unitHeight
-    const toX = this.offset + x2 * this.unitWidth
-    const toY = this.offset + y2 * this.unitHeight
-    this.ctx.moveTo(fromX, fromY)
-    this.ctx.lineTo(toX, toY)
-  }
-
-  drawText(text, x, y) {
-    const left = x * this.unitWidth + this.offset
-    const top = y * this.unitHeight + this.offset
-    this.ctx.fillStyle = 'black'
-    this.ctx.fillText(text, left, top)
-  }
-}
+import Renderer from './Renderer'
 
 export default {
   name: 'maze',
-  data() {
+  data () {
     return {
       width: null,
       height: null,
@@ -101,7 +36,8 @@ export default {
         x: 0,
         y: 0
       },
-      isFinished: false
+      isFinished: false,
+      seed: Date.now()
     }
   },
   computed: {
@@ -274,8 +210,9 @@ export default {
     resetMaze () {
       const lx = this.lx
       const ly = this.ly
+      const seed = this.seed++
       if (lx > 0 && ly > 0) {
-        const maze = new Maze(lx, ly, seed++)
+        const maze = new Maze(lx, ly, seed)
         Vue.set(this, 'maze', maze)
         Vue.set(this, 'player', { x: 0, y: 0 })
         this.isFinished = false
